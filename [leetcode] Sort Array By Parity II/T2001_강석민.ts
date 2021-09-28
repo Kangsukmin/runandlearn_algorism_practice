2faeaa7d8313d04e880f558c355838e028eeff37
kangsukmin <50981395+Kangsukmin@users.noreply.github.com>
2021-09-28 19:51:52 +0900

    refactor: isParity => isParityMatch

--- [leetcode] Sort Array By Parity II/T2001_강석민.ts
+++ [leetcode] Sort Array By Parity II/T2001_강석민.ts
@@ -1,12 +1,12 @@
-function isParity(index: number, value: number): boolean {
+function isParityMatch(index: number, value: number): boolean {
     return (index % 2) === (value % 2);
 }
 
 function sortArrayByParityII(nums: number[]): number[] {
     let oddIndex = 1
     for(let evenIndex=0; evenIndex < nums.length; evenIndex += 2) {
-        while (!isParity(evenIndex, nums[evenIndex])) {
-            if(!isParity(oddIndex, nums[oddIndex])) {
+        while (!isParityMatch(evenIndex, nums[evenIndex])) {
+            if(!isParityMatch(oddIndex, nums[oddIndex])) {
                 [nums[evenIndex], nums[oddIndex]] = [nums[oddIndex], nums[evenIndex]];
             }
             oddIndex += 2;
