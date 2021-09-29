6fb49bdc6cd77fe8ca4cbee4bd369c96ec8a4597
TaeHyeon Kim <xogus9641@naver.com>
2021-09-29 13:22:28 +0900

    풀이 추가

---
+++ [leetcode] Sort Array By Parity II/T2066_김태현.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        result = [-1]*len(nums)
+        
+        even_idx = 0
+        odd_idx = 1
+        
+        for num in nums:
+            if num%2 == 0:
+                result[even_idx] = num
+                even_idx += 2
+            else:
+                result[odd_idx] = num
+                odd_idx += 2
+        
+        return result
