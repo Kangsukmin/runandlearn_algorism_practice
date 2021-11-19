88b9e7b81eeeb1ed4a7eb6f9d01cc3b8f284a739
deCyma <50981395+Kangsukmin@users.noreply.github.com>
2021-11-19 18:32:45 +0900

    Update T2001_강석민.ts

--- [211119 - leetcode] 461. Hamming Distance/T2001_강석민.ts
+++ [211119 - leetcode] 461. Hamming Distance/T2001_강석민.ts
@@ -1,14 +1,13 @@
 function hammingDistance(x: number, y: number): number {
     const [xLog, yLog] = [Math.log2(x), Math.log2(y)];
-    let exp = Math.max(Math.floor(xLog), Math.floor(yLog));
+    const exp = Math.max(Math.floor(xLog), Math.floor(yLog));
+    let div = 2 ** exp;
     let count = 0
-    while (exp >= 0) {
-        const div = 2 ** exp;
-        const [x_val, y_val] = [Math.floor(x / div), Math.floor(y / div)];
+    while (div >= 1) {
+        if (Math.floor(x / div) !== Math.floor(y / div)) count += 1
         x %= div
         y %= div
-        if (x_val !== y_val) count += 1
-        exp -= 1
+        div /= 2
     }
     return count;
 };
