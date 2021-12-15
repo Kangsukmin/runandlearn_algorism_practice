170d9586d3a130bcb387da7484cf8dfcfc5e2cc2
TaeHyeon Kim <xogus9641@naver.com>
2021-12-15 17:28:20 +0900

    Create T2066_김태현.py

---
+++ Leetcode - Algorithm Project/Day05/T2066_김태현.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        left = right = head
+        
+        for _ in range(n):
+            right = right.next
+        
+        while right.next != None:
+            left = left.next
+            right = right.next
+        
+        left.next = left.next.next
+        return head
